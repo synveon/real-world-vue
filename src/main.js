b4a1aabab911c5f73b7bfff8ed7c56cd16eb63b8
Kevin Laptop <senzero@gmail.com>
2019-02-24 19:46:58 -0700

    Finished entire module

--- src/main.js
+++ src/main.js
@@ -1,12 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+/* ------ START: Automatic global component regristion code ---------------- */
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
-Vue.config.productionTip = false;
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
+
+/* ------ END: Automatic global component regristion code ------------------ */
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/router.js
+++ src/router.js
@@ -1,26 +1,35 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import EventList from './views/EventList.vue'
+//import EventShow from './views/EventShow.vue'
+//import EventCreate from './views/EventCreate.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/',
+      name: 'event-list',
+      component: EventList
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/event/:id',
+      name: 'event-show',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "event-show" */ './views/EventShow.vue'),
+      props: true
+    },
+    {
+      path: '/event/create',
+      name: 'event-create',
+      component: () =>
+        import(/* webpackChunkName: "event-create" */ './views/EventCreate.vue')
     }
   ]
-});
+})
